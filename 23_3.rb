6bbddd6c87b7061e11cbdb1e7da3c43e4ff11dfa
Inga <guingg@inbox.ru>
2018-08-14 03:30:36 -0700

    correction

--- 23_3.rb
+++ 23_3.rb
@@ -1,20 +1,16 @@
 require 'digest'
 
 puts "Введите фразу"
-
 user_input = STDIN.gets.chomp
 
 puts "Выберете способ зашифровки: 1. SHA1; 2. MD5. (Введите 1 или 2)"
-
 user_input2 = STDIN.gets.to_i
 
 puts "Вот что получилось: "
-
 if user_input2 == "1"
   puts Digest::SHA1.hexdigest '#{user_input2}'
 else
   puts Digest::MD5.hexdigest'#{user_input2}'
-
 end
 
 
